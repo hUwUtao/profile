7655eaddea40370f9be1c60cce9cdbc52848b8b5
stdpi <iam@stdpi.work>
2024-11-07 23:25:28 +0700

    gyatt

--- astro.config.ts
+++ astro.config.ts
@@ -6,36 +6,36 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	// adapter: cloudflare({ mode: "directory" }),
-	vite: {
-		resolve: {
-			alias: [...(process.env.NODE_ENV === "production" ? esmsh() : [])],
-		},
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: "shiki",
-			shikiConfig: {
-				theme: "monokai",
-			},
-		}),
-		react(),
-		process.env.NODE_ENV === "production" && compress(),
-	].filter(Boolean),
+  output: "static",
+  // adapter: cloudflare({ mode: "directory" }),
+  vite: {
+    resolve: {
+      // alias: [...(process.env.NODE_ENV === "production" ? esmsh() : [])],
+    },
+  },
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "monokai",
+      },
+    }),
+    react(),
+    process.env.NODE_ENV === "production" && compress(),
+  ].filter(Boolean),
 });
 function esmsh() {
-	return [
-		//
-		"react",
-		"react-dom/client",
-		"react-router-dom",
-		"@sentry/integrations",
-		"@sentry/react",
-		"canvas-confetti",
-		"bootstrap-icons",
-	].map((k) => ({
-		find: new RegExp(`^(${k}[/]?.*)$`),
-		replacement: "https://esm.sh/$1?bundle",
-	}));
+  return [
+    //
+    "react",
+    "react-dom/client",
+    "react-router-dom",
+    "@sentry/integrations",
+    "@sentry/react",
+    "canvas-confetti",
+    "bootstrap-icons",
+  ].map((k) => ({
+    find: new RegExp(`^(${k}[/]?.*)$`),
+    replacement: "https://esm.sh/$1?bundle",
+  }));
 }
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 // type KVNamespace = import("@cloudflare/workers-types/experimental").KVNamespace;
 // type ENV = {
